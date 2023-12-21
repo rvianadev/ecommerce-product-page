f87b599fd55813011d5867e39cdc666dfefaff7c
Rodrigo Viana <roviol@gmail.com>
2023-12-21 10:13:19 -0300

    feat: product section wip

--- src/components/ProductSection/index.js
+++ src/components/ProductSection/index.js
@@ -3,8 +3,69 @@ import styles from "./styles.module.css";
 function ProductSection() {
   return (
     <section className={styles.productSection}>
-      <article className={styles.productCards}></article>
-      <aside className={styles.productInformations}></aside>
+      <article className={styles.productCards}>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed neque ex,
+          scelerisque eu convallis sit amet, ornare at ipsum. Donec sit amet
+          quam dignissim, tristique tellus at, blandit mi. Vestibulum eu tortor
+          non purus vestibulum suscipit. Aliquam facilisis nibh sed consequat
+          sodales. In vel volutpat massa. Vivamus lacinia tellus diam, blandit
+          finibus erat mattis et. Nam efficitur est placerat iaculis feugiat.
+          Praesent mattis, odio id convallis lacinia, lacus justo mattis magna,
+          commodo vestibulum elit tellus eget libero. Maecenas dictum mauris
+          pulvinar, accumsan sem vitae, tempor elit. Sed rhoncus lectus ut enim
+          ullamcorper, et euismod purus posuere. Pellentesque varius neque non
+          est laoreet, vitae malesuada neque feugiat. Praesent vestibulum cursus
+          tortor et efficitur. Pellentesque pellentesque sit amet massa ut
+          fermentum. Sed scelerisque pharetra arcu, id laoreet ex porta id.
+          Pellentesque at turpis gravida, consectetur neque et, blandit purus.
+          Praesent nibh lorem, porttitor pretium lorem sit amet, dapibus auctor
+          arcu. Nullam auctor eros eget urna luctus, ac posuere nisi efficitur.
+          Integer suscipit auctor nisl, id dignissim elit iaculis eget. Fusce
+          elementum vehicula euismod. Phasellus vitae risus eget ex ornare
+          cursus a nec libero. Curabitur aliquet faucibus quam, non elementum
+          purus convallis non. Praesent facilisis consequat orci quis luctus. In
+          hac habitasse platea dictumst. Praesent molestie, justo non vehicula
+          laoreet, lectus turpis fringilla quam, sed mattis ex quam non augue.
+          Nulla id porta est, sit amet sagittis libero. Etiam volutpat velit non
+          porta vestibulum. Pellentesque vel purus consectetur, feugiat neque
+          sed, dictum quam. In et pharetra felis, at feugiat diam. Aliquam erat
+          volutpat. Vestibulum non purus consequat, hendrerit dui a, tempus
+          nibh. Ut ac tellus urna. Aenean consectetur ex eget nisl mattis, vitae
+          mattis urna rutrum. Nunc cursus nisl lectus, ac lobortis mi vehicula
+          quis. Nam id ex vitae massa vulputate tempor tempus ac elit.
+          Suspendisse potenti. Mauris condimentum ullamcorper nunc, at fermentum
+          dolor interdum dignissim. Mauris ac tincidunt neque, at finibus nulla.
+          Maecenas condimentum dignissim arcu, sit amet sagittis odio viverra
+          eget. Nunc euismod sodales sem.
+        </p>
+      </article>
+      <aside className={styles.productInformations}>
+        <p>
+          Aenean scelerisque velit eget erat lacinia, suscipit bibendum dui
+          elementum. Nam convallis consectetur semper. Pellentesque eu pulvinar
+          est. Praesent turpis lectus, auctor ac maximus sit amet, egestas vel
+          quam. Suspendisse imperdiet massa dignissim arcu pharetra, et
+          porttitor mi rutrum. Praesent convallis erat vehicula nibh bibendum
+          commodo. In sit amet dui at arcu tempus malesuada. Praesent posuere,
+          erat et hendrerit lacinia, lacus nunc dignissim velit, sed tempus
+          risus risus et erat. Cras sit amet felis nec ipsum luctus varius.
+          Maecenas ultricies volutpat libero et tempus. Nunc et elit finibus,
+          facilisis quam eget, pellentesque urna. Nunc et convallis orci.
+          Praesent posuere libero nibh, eu ultricies nunc sollicitudin vitae.
+          Phasellus elementum, turpis vitae fermentum semper, elit tellus porta
+          purus, id sollicitudin ipsum dolor ac tortor. Curabitur sit amet
+          sollicitudin erat. Mauris ullamcorper a erat vel condimentum. Vivamus
+          laoreet erat eu finibus facilisis. Proin efficitur ornare mi quis
+          pulvinar. Donec tempor massa nisi, sit amet tincidunt justo ultrices
+          sit amet. Praesent nec fermentum erat, ac pellentesque magna. Morbi
+          fringilla nibh turpis, non luctus ligula faucibus sed. Quisque
+          scelerisque sed nisl at rhoncus. Praesent id tellus vel purus aliquam
+          elementum. Integer ultrices sagittis lorem, sed dapibus nisl. Donec
+          lectus risus, eleifend in posuere et, tincidunt non diam. Nunc laoreet
+          quam eu orci auctor, quis facilisis mi ullamcorper.
+        </p>
+      </aside>
     </section>
   );
 }
