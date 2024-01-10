ff992e53ec21ad92f97e5731ec718bf5410dba65
Rodrigo Viana <roviol@gmail.com>
2024-01-10 18:34:10 -0300

    refactor: routes

--- src/pages/productAPI.js
+++ src/pages/productAPI.js
@@ -1,12 +1,21 @@
+import { useParams } from "react-router-dom";
+
 import Container from "../components/Container";
 import Header from "../components/Header";
 
 import CartModal from "../components/CartModal";
 import { CartProvider } from "../contexts/Cart";
+import api from "../api/api";
 
 import "../globals.css";
 
 function ProductAPI() {
+  const { productId } = useParams();
+
+  const getProduct = async () => {
+    api.get();
+  };
+
   return (
     <Container>
       <CartProvider>
