0b9e635fa8b6663828c799113da9681a98076c18
Rodrigo <roviol@gmail.com>
2024-01-11 21:30:38 -0300

    refactor

--- src/pages/productAPI.js
+++ src/pages/productAPI.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import { useParams } from "react-router-dom";
 
 import Container from "../components/Container";
@@ -5,17 +6,9 @@ import Header from "../components/Header";
 
 import CartModal from "../components/CartModal";
 
-import api from "../api/api";
-
 import "../globals.css";
 
 function ProductAPI() {
-  const { productId } = useParams();
-
-  const getProduct = async () => {
-    api.get();
-  };
-
   return (
     <Container>
       <Header />
