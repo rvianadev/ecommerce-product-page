baad26afcb990d65e9716c9205ccb2e759646c29
Rodrigo <roviol@gmail.com>
2024-01-11 22:56:28 -0300

    refactor: almost done

--- src/pages/productAPI.js
+++ src/pages/productAPI.js
@@ -1,14 +1,26 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import { useParams } from "react-router-dom";
+import { useState } from "react";
 
 import Container from "../components/Container";
 import Header from "../components/Header";
 
 import CartModal from "../components/CartModal";
+import api from "../api/api";
 
 import "../globals.css";
 
 function ProductAPI() {
+  const productId = useParams();
+
+  const [product, setProduct] = useState([]);
+
+  const getProduct = async () => {
+    await api.get(`photos?id=${productId}`).then((response) => {
+      setProduct(response.data);
+    });
+  };
+
   return (
     <Container>
       <Header />
