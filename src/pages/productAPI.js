a27bc8d423923df423d456f43aae96867f6022c2
Rodrigo Viana <roviol@gmail.com>
2024-01-12 08:02:22 -0300

    feat: product page

--- src/pages/productAPI.js
+++ src/pages/productAPI.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import Container from "../components/Container";
@@ -11,15 +12,32 @@ import "../globals.css";
 
 function ProductAPI() {
   const { productId } = useParams();
+  const [url, setUrl] = useState(null);
 
-  const getProduct = async () => {
-    api.get();
+  const productURL = async () => {
+    try {
+      const response = await api.get(`photos?id=${productId}`);
+      return response.data[0].url;
+    } catch (error) {
+      console.error("Erro ao obter URL do produto:", error);
+      return null;
+    }
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const fetchedUrl = await productURL();
+      setUrl(fetchedUrl);
+    };
+
+    fetchData();
+  }, [productId]);
 
   return (
     <Container>
       <Header />
       <CartModal />
+      {url && <img src={url} alt="Imagem do produto" />}
     </Container>
   );
 }
