07588f085c01afc91a08080753b43202dfecf68b
Rodrigo Viana <roviol@gmail.com>
2024-01-12 11:44:49 -0300

    feature: product API page finished

--- src/pages/productAPI.js
+++ src/pages/productAPI.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 
 import Container from "../components/Container";
@@ -23,20 +23,35 @@ function ProductAPI() {
     }
   };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const fetchedUrl = await productURL();
-      setUrl(fetchedUrl);
-    };
+  const fetchData = async () => {
+    const fetchedUrl = await productURL();
+    setUrl(fetchedUrl);
+  };
 
-    fetchData();
-  }, [productId]);
+  fetchData();
 
   return (
     <Container>
       <Header />
       <CartModal />
-      {url && <img src={url} alt="Imagem do produto" />}
+
+      {url && (
+        <div
+          style={{
+            paddingTop: "5.62rem",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <h1 style={{ marginBottom: "2rem" }}>Product {productId}</h1>
+          <img
+            src={url}
+            alt="Imagem do produto"
+            style={{ borderRadius: "0.9375rem" }}
+          />
+        </div>
+      )}
     </Container>
   );
 }
